08061fe2f47773cebd3ea9849ab7fe2da9ad50fe
Hemang Rajyaguru <hemdagem@gmail.com>
2017-01-22 13:27:17 +0000

    moved h1 to header and removed black font text

--- _site/assets/js/setup.js
+++ _site/assets/js/setup.js
@@ -9,11 +9,6 @@ $(document).ready(function(){
     }
 });
 
-// carousel setup
-//if($('.carousel').length > 0){
-//    $('.carousel').carousel();
-//}
-
 //load images after page load
  $('.responsive-img[data-src]').each(function() { 
      $(this).attr('src', $(this).data('src')); 
